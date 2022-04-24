c68ab368d475130f581e9d3fb9b7e03109c0fa51
Alex <45256796+RPG-Alex@users.noreply.github.com>
2022-04-24 20:20:44 +0800

    Basic upload functionality is working (just from php manual as of now)

--- upload.php
+++ upload.php
@@ -1,2 +1,36 @@
 <?php
+// dev stuff delete in production
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+//
+
+
 //This file will contain the protocol for uploading files
+
+//This is from the php manual. Need to modify to work only with 3 file tipes and allow for adding subidectories or adding to sub direcotries (CSS, JS, etc)
+$uploaddir = '/var/www/html/static-uploader/';
+$uploadfile =  $uploaddir.basename($_FILES['userfile']['name']);
+
+echo '<pre>';
+if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
+    echo "File is valid, and was successfully uploaded.\n";
+} else {
+    echo "Possible file upload attack!\n";
+}
+
+echo 'Here is some more debugging info:';
+print_r($_FILES);
+
+print "</pre>";
+
+?>
+
+<!-- The data encoding type, enctype, MUST be specified as below -->
+<form enctype="multipart/form-data" action="" method="POST">
+    <!-- MAX_FILE_SIZE must precede the file input field -->
+    <input type="hidden" name="MAX_FILE_SIZE" value="30000" />
+    <!-- Name of input element determines name in $_FILES array -->
+    Send this file: <input name="userfile" type="file" />
+    <input type="submit" value="Send File" />
+</form>
